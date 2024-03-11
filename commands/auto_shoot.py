f3e5aab466a00d41276f2bd2e8c356010c62aa20
Aram Sodhi <93051510+aramsodhi@users.noreply.github.com>
2024-03-11 12:44:50 -0700

    REDO auto shoot code for work during meeting during driving practice

--- commands/auto_shoot.py
+++ commands/auto_shoot.py
@@ -1,33 +1,17 @@
 #import math for arctan, wpilib for the timer
 import math, wpilib
 
-#import Shooter so we can spin the shooter motors
-from subsystems.shooter import Shooter
-
-#import IMU so we can use the imu on the arm
-from subsystems.imu import IMU
-
-#importing PID for various functions
-from utils import pid
 class Auto_Shoot:
     def __init__(self, _arm, _drive, _shooter, _intake, _imu, _networking):
         #different stages of shooting
-        self.SHOOTER_IDLE = 0 #idle, not doing anyting
-        self.ANGLE_SCAN = 1 #scan the apriltag on the speaker, get the angle that it is at
-        self.ANGLE_ROBOT = 2 #angle the robot to center it on the tag
-        self.DISTANCE_SCAN = 3 #scan the distance between the robot and the tag
-        self.ARM_ANGLE_CALC = 4 #find the current angle of the arm, calculate needed angle for the arm
-        self.ARM_ANGLE_MOVE = 5 #move the arm to the angle that we had calculated
-        self.SHOOTER_MOTOR_SPIN = 6 #start spinning the shooter motors for about one second
-        self.FEED_NOTE = 7 #feed the note into the shooter motors, firing the note
-        self.RETURN_ARM = 8 #move the arm back down to the normal angle.
-        self.SHOOTER_DONE = 9 #done with everything
+        self.IDLE = 0 #idle, not doing anyting
+        self.CENTERING_ROBOT = 1 # move robot until apriltag is in center of screen
+        self.ARM_TO_ANGLE = 2 
+        self.SHOOTER_MOTOR_SPIN = 3 #start spinning the shooter motors for about 1.5-1.75 seconds
+        self.FEED_NOTE = 4 #feed the note into the shooter motors, firing the note
+        self.RETURN_ARM = 5 # move the arm back to position inside the robot
+        self.FINISHED = 6 #done with everything
         self.shooter_stage = self.SHOOTER_IDLE #set the current state to the idle state
-
-
-        # intermediate variables that we are using between stages in our state machine
-        self.april_tag_x = None
-        self.arm_angle = None
 
         #reference to the arm that we passed in
         self.arm = _arm
@@ -47,133 +31,56 @@ class Auto_Shoot:
         #reference to networking
         self.networking = _networking
         
-        #initializing pid for shooter
-        #zeroes are current placeholders that will be modified after tuning
-        self.shooter_P = 0
-        self.shooter_I = 0
-        self.shooter_D = 0
-        self.arm_val = 0
-        self.shooter_pid = pid.PID(self.shooter_P, self.shooter_I, self.shooter_D, self.arm_val)
-
         #timer to track how long motors have been spinning and if we can move on to the next stage.
         self.timer = wpilib.Timer()
         self.shooter_spin_start_time = 0.0
         self.feeding_spin_start_time = 0.0
 
-        #intermediate variable for apriltag data
-        self.apriltag_data = None
-        self.apriltag_distance = None
-
-    #scans the apriltag for the apriltag data
-    #turns the robot to center it with the speaker apriltag.
-    def center_robot(self):
-        self.apriltag_data = self.networking.get_apriltag_data()
-
-        if len(self.apriltag_data) is not None:
-            april_tag_x = self.apriltag_data[0]
-            if abs(self.note_x) < 3: return True
-            elif self.note_x < -3: self.drive.tank_drive(-.5, .5)
-            elif self.note_x > 3: self.drive.tank_drive(.5, -.5)
-        else: return False
-    #raspi scans distance between bot and speaker(apriltag)
-    def get_distance(self):
-        self.apriltag_data = None
-        self.apriltag_data = self.networking.get_apriltag_data()
-        if len(self.apriltag_data) is not None:
-            self.apriltag_distance = self.apriltag_data[2]
-            return True
-        else: return False
-        
-    def arm_angle_calc(self):
-        #UNITS ARE IN INCHES
-        #distance from raspi camera to apriltag
-        #10 is a placeholder
-        april_tag_distance = 100
-
-        #height of the arm when shooting.
-        arm_height = 18.5
-        speaker_height = 80.7
-
-        #our math formula for the angle that the arm needs to be set at ATAN = ARCTAN
-        shooting_angle = math.atan((speaker_height - arm_height) / april_tag_distance)
-        return True
-    
-    def arm_angle_move(self):
-        """
-        get values for our current arm angle 
-        also get the value that we want to move the arm to
-        this is where an imu would be really cool!
-        run PID, move arm motors according to that
-        if same angle:
-            return true
-        else:
-            return false
-        """
-
-    #spins the shooter motor before for a second before we feed the note in.
-    def spinning_shooter_motor(self):
-        #starts spinning the shooter motors for one second
-        self.shooter.shooter_spin(1)
-
-        #timer to check if it's been a second
-        #if a second has passed, we return true
-        if self.shooter_spin_start_time + 1 < self.timer.getFPGATimestamp(): return True
-    
-    def feed_note(self):
-        #keeps the shooter motors spinning
-        self.shooter.shooter_spin(1)
-
-        #spins the intake motors to feed the note into the shooter motors
-        self.intake.intake_spin(1)
-        
-        #if a second has passed, it will have most likely shot, so we can move on to the next step
-        if self.feeding_spin_start_time + 1 < self.timer.getFPGATimestamp(): 
-            #stops shooter motors
-            self.shooter.stop()
-            return True
-
-    def return_arm():
-        """
-        move arm back to orignal position
-        """
     def autonomous_shoot(self):
-        if self.shooter_stage == self.SHOOTER_IDLE:
-            self.shooter_stage = self.ANGLE_SCAN
+        if self.shooter_stage == self.IDLE:
+            self.shooter_stage = self.CENTERING_ROBOT
 
-        elif self.shooter_stage == self.ANGLE_SCAN:
-            if self.scan_for_april_tags():
-                self.shooter_stage = self.ANGLE_ROBOT
+        elif self.shooter_stage == self.CENTERING_ROBOT:
+            # get position of april tag (x)
+            # if the apriltag x position is negative, rotate robot left
+            # else if april tag position is positive, rotate robot right
+            # if it is within tolerance band, move to next stage
+                
+            self.shooter_stage = self.ARM_TO_ANGLE
+    
+        elif self.shooter_stage == self.ARM_TO_ANGLE:
+            # get distance of apriltag
+            # convert to same units as all other numbers
+            # calculate arm height
+            # run arctan calculation to get angle
+            # justify for angle of shooter relative to arm
+            # move arm to the angle
+            # once arm is within a certain degree of tolerance, move to next step OR after certain amount of time if tolerance if not consistent
+            # set holding position and everything
+            
+            self.shooter_stage = self.SHOOTER_MOTOR_SPIN
 
-        elif self.shooter_stage == self.ANGLE_ROBOT:
-            #turn the robot using the values of the apriltag scan
-            if self.turn_robot():
-                self.shooter_stage = self.DISTANCE_SCAN
-
-        elif self.shooter_stage == self.DISTANCE_SCAN:
-            if self.distance_scan():
-                self.shooter_stage = self.ARM_ANGLE_CALC
-
-        elif self.shooter_stage == self.ARM_ANGLE_CALC:
-            if self.arm_angle_calc():
-                self.shooter_stage = self.ARM_ANGLE_MOVE
-
-        elif self.shooter_stage == self.ARM_ANGLE_MOVE:
-            if self.arm_angle_move():
-                self.shooter_stage = self.SHOOTER_MOTOR_SPIN
-                self.shooter_spin_start_time = self.timer.getFPGATimestamp()
-
-        elif self.shooter_stage == self.SHOOTER_MOTOR_SPIN:
-            if self.spinning_shooter_motor():
-                self.shooter_stage = self.FEED_NOTE
-                self.feeding_spin_start_time = self.timer.getFPGATimestamp()
+        elif self.shooter_stage == self.SHOTER_MOTOR_SPIN:
+            # spin shooter motor for 1.5 seconds
+            # if 1.5 seconds is up, move to feeding
+            
+            self.shooter_stage = self.FEED_NOTE
 
         elif self.shooter_stage == self.FEED_NOTE:
-            if self.feed_note():
-                self.shooter_stage = self.RETURN_ARM
-        
-        elif self.shooter_stage == self.RETURN_ARM:
-            if self.return_arm():
-                self.shooter_stage = self.SHOOTER_DONE
+            # spin shooter and intake for 1.5 seconds
+            # if 1.5 seconds is up, move to returning arm
+            
+            self.shooter_stage = self.RETURN_ARM:
 
-        elif self.shooter_stage == self.SHOOTER_DONE:
-            self.shooter_stage = self.SHOOTER_IDLE
+        elif self.shooter_stage == self.RETURN_ARM:
+            # move arm to position that is in the chassis
+            # if within tolerance OR timer, move to finished
+            
+            self.shooter_stage = self.FINISHED
+
+        elif self.shooter_stage == self.FINISHED:
+            # reset
+            # alert driver ready to start sequence again
+            # go to idle ?
+            
+            pass
